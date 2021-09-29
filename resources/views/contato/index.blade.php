78d36f2897d3197b94be702acfd30dc0e6c671e3
Francisco Dias de Asis Pereira <francisco.pereira@bit.local>
2021-09-29 18:21:04 -0300

    Backend confirmação de delete.

--- resources/views/contato/index.blade.php
+++ resources/views/contato/index.blade.php
@@ -26,29 +26,7 @@
                         </div>
                     @endforeach
                 </div>
-                    <!-- Button trigger modal -->
-                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-                        Launch demo modal
-                    </button>
 
-                    <!-- Modal -->
-                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                                    <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    Deseja relamente deletar esse contao?
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                    <button type="button" class="btn btn-primary">Save changes</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
 
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
                 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
--- resources/views/layouts/pills.blade.php
+++ resources/views/layouts/pills.blade.php
@@ -1,16 +1,44 @@
+ <!-- Button trigger modal
+ <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+    Launch demo modal
+</button>
+ -->
 <div class="dropdown">
     <button class="btn-sm btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         Ações
     </button>
     <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
         <a class="dropdown-item" href="{{route('contatos.edit', $contato->id)}}">Editar</a>
-            <form method="post" action="{{route('contatos.destroy',$contato->id)}}" class="form-horizontal">
-                {{csrf_field()}}
-                <input type="hidden" name="_method" value="DELETE">
-                <button type="submit" class="dropdown-item">
-                    Delete
-                </button>
-            </form>
 
+         <!-- Button trigger modal -->
+        <button type="button" class="dropdown-item" data-toggle="modal" data-target="#exampleModal">Detetar</button>
     </div>
 </div>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel"> DELETE </h5>
+                <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                Deseja relamente deletar o contato: {{ $contato->name }}?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <form method="post" action="{{route('contatos.destroy',$contato->id)}}" class="form-horizontal">
+                    {{csrf_field()}}
+                    <input type="hidden" name="_method" value="DELETE">
+                    <button type="submit" class="btn btn-primary">Save changes</button>
+                </form>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
