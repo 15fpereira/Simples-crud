879c09fd32e9429c7a36bbd00c041158ab903182
Francisco Dias de Asis Pereira <francisco.pereira@bit.local>
2021-09-30 13:27:05 -0300

    Correção de bug na confirmação de delete

--- resources/views/layouts/pills.blade.php
+++ resources/views/layouts/pills.blade.php
@@ -11,16 +11,16 @@
         <a class="dropdown-item" href="{{route('contatos.edit', $contato->id)}}">Editar</a>
 
          <!-- Button trigger modal -->
-        <button type="button" class="dropdown-item" data-toggle="modal" data-target="#exampleModal">Detetar</button>
+        <button type="button" class="dropdown-item" data-toggle="modal" data-target="#exampleModal{{ $loop->iteration }}">Detetar</button>
     </div>
 </div>
 
 <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="exampleModal{{ $loop->iteration }}" tabindex="-1" aria-labelledby="exampleModalLabel{{ $loop->iteration }}" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel"> DELETE </h5>
+                <h5 class="modal-title" id="exampleModalLabel{{ $loop->iteration }}"> DELETE </h5>
                 <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
@@ -31,7 +31,7 @@
                 <form method="post" action="{{route('contatos.destroy',$contato->id)}}" class="form-horizontal">
                     {{csrf_field()}}
                     <input type="hidden" name="_method" value="DELETE">
-                    <button type="submit" class="btn btn-primary">Save changes</button>
+                    <button type="submit" class="btn btn-primary">Deletar</button>
                 </form>
 
             </div>
